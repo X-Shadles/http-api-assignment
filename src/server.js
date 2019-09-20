7fc607d42674e0ad8f47f177082c66e86d34187e
Ethan Henson (RIT Student) <esh9426@ad.rit.edu>
2019-09-20 11:45:27 -0400

    Pulled from class notes

--- src/server.js
+++ src/server.js
@@ -1,39 +1,34 @@
     
 const http = require('http');
+const url = require('url'); 
 
-const htmlHandler = require('./htmlResponses.js');
-const mediaHandler = require('./mediaResponses.js');
+const query = require('querystring'); 
+const htmlHandler = require('./htmlResponses.js'); 
+const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const onRequest = (request, response) => {
-  console.log(request.url);
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/success': jsonHandler.success,
+  '/badRequest': jsonHandler.badRequest,
+  '/unauthorized': jsonHandler.badRequest,
+  '/forbidden': jsonHandler.badRequest,
+  '/internal': jsonHandler.badRequest,
+  '/notImplemented': jsonHandler.badRequest,
+  notFound: jsonHandler.notFound,
+};
 
-  switch (request.url) {
-    case '/':
-      htmlHandler.getIndex(request, response, '../client/client.html');
-      break;
-    case '/success':
-      mediaHandler.getVideo(request, response, '../client/party.mp4', 'video/mp4');
-      break;
-    case '/badRequest':
-      htmlHandler.getIndex(request, response, '../client/client2.html');
-      break;
-    case '/unauthorized':
-      mediaHandler.getVideo(request, response, '../client/bling.mp3', 'audio/mpeg');
-      break;
-    case '/forbidden':
-      htmlHandler.getIndex(request, response, '../client/client3.html');
-      break;
-    case '/internal':
-      mediaHandler.getVideo(request, response, '../client/bird.mp4', 'video/mp4');
-      break;
-    case '/notImplemented':
-        mediaHandler.getVideo(request, response, '../client/bird.mp4', 'video/mp4');
-        break;
-    default:
-      htmlHandler.getIndex(request, response, '../client/client.html');
-      break;
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  const params = query.parse(parsedUrl.query);
+
+  if (urlStruct[parsedUrl.pathname]) {
+
+    urlStruct[parsedUrl.pathname](request, response, params);
+  } else {
+
+    urlStruct.notFound(request, response, params);
   }
 };
 
