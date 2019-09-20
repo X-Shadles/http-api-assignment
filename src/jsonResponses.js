ba05f8641ae0e87b7578d07b1593c3905212cbbb
Ethan Henson (RIT Student) <esh9426@ad.rit.edu>
2019-09-20 13:44:00 -0400

    Getting Help

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,89 +1,93 @@
 const respondJSON = (request, response, status, object) => {
-    response.writeHead(status, { 'Content-Type': 'application/json' });
-    response.write(JSON.stringify(object));
-    response.end();
+  response.writeHead(status, {
+    'Content-Type': 'application/json'
+  });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+const success = (request, response) => {
+  const responseJSON = {
+    message: 'This is a successful response',
+    id: 'success',
   };
-  
-  const success = (request, response) => {
-    const responseJSON = {
-      message: 'This is a successful response',
-    };
-    respondJSON(request, response, 200, responseJSON);
-  };
-  
-  const badRequest = (request, response, params) => {
-    const responseJSON = {
-      message: 'This request has the required parameters',
-      id: 'notBadRequest',
-    };
-  
-    if(!params.valid || params.valid !== 'true') {
-      responseJSON.message = 'Missing valid query parameter set to true';
-      responseJSON.id = 'badRequest';
-      return respondJSON(request, response, 400, responseJSON);
-    }
-  
-    return respondJSON(request, response, 200, responseJSON);
+  respondJSON(request, response, 200, responseJSON);
+};
+
+const badRequest = (request, response, params) => {
+  const responseJSON = {
+    message: 'This request has the required parameters',
+    id: 'notBadRequest',
   };
 
-  const unauthorized = (request, response, params) => {
-      const responseJSON ={
-          message: 'This request has the required parameters',
-      };
-
-      if(!params.loggedIn || params.loggedIn !== 'yes') {
-        responseJSON.message = 'Missing valid query parameter set to yes';
-        responseJSON.message = 'You need to log in';
-        responseJSON.id = 'unauthorized';
-        return respondJSON(request, response, 401, responseJSON);
-      }
-
-      respondJSON(request, response, 200, responseJSON);
+  if (!params.valid || params.valid !== 'true') {
+    responseJSON.message = 'Missing valid query parameter set to true';
+    responseJSON.id = 'badRequest';
+    return respondJSON(request, response, 400, responseJSON);
   }
 
-  const forbidden = (request, response) => {
-    const responseJSON = {
-      message: 'Hey, you can\'t be here!',
-      id: 'forbidden',
-    };
-  
-    respondJSON(request, response, 403, responseJSON);
+  return respondJSON(request, response, 200, responseJSON);
+};
+
+const unauthorized = (request, response, params) => {
+  const responseJSON = {
+    message: 'This request has the required parameters',
+    id: 'authorized'
   };
 
-  const internal = (request, response) => {
-    const responseJSON = {
-      message: 'Internal Server Error',
-      id: 'forbidden',
-    };
-  
-    respondJSON(request, response, 500, responseJSON);
-  };
-  
-  const notImplemented = (request, response) => {
-    const responseJSON = {
-      message: 'Status code for this is not implemented',
-      id: 'notImplemented',
-    };
-  
-    respondJSON(request, response, 501, responseJSON);
+  if (!params.loggedIn || params.loggedIn !== 'yes') {
+    responseJSON.message = 'Missing valid query parameter set to yes';
+    responseJSON.message = 'You need to log in';
+    responseJSON.id = 'unauthorized';
+    return respondJSON(request, response, 401, responseJSON);
+  }
+
+  respondJSON(request, response, 200, responseJSON);
+}
+
+const forbidden = (request, response) => {
+  const responseJSON = {
+    message: 'Hey, you can\'t be here!',
+    id: 'forbidden',
   };
 
-  const notFound = (request, response) => {
-    const responseJSON = {
-      message: 'The page you are looking for was not found!!',
-      id: 'notFound',
-    };
-  
-    return respondJSON(request, response, 404, responseJSON);
-  };
-  
+  respondJSON(request, response, 403, responseJSON);
+};
 
-  module.exports = {
-    success,
-    badRequest,
-    forbidden,
-    unauthorized,
-    internal,
-    notImplemented,
-    notFound,
+const internal = (request, response) => {
+  const responseJSON = {
+    message: 'Internal Server Error',
+    id: 'internal',
   };
+
+  respondJSON(request, response, 500, responseJSON);
+};
+
+const notImplemented = (request, response) => {
+  const responseJSON = {
+    message: 'Status code for this is not implemented',
+    id: 'notImplemented',
+  };
+
+  respondJSON(request, response, 501, responseJSON);
+};
+
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found!!',
+    id: 'notFound',
+  };
+
+  return respondJSON(request, response, 404, responseJSON);
+};
+
+
+module.exports = {
+  success,
+  badRequest,
+  forbidden,
+  unauthorized,
+  internal,
+  notImplemented,
+  notFound,
+};
--- src/server.js
+++ src/server.js
@@ -1,37 +1,35 @@
-    
-const http = require('http');
-const url = require('url'); 
+    const http = require('http');
+    const url = require('url');
 
-const query = require('querystring'); 
-const htmlHandler = require('./htmlResponses.js'); 
-const jsonHandler = require('./jsonResponses.js');
+    const htmlHandler = require('./htmlResponses.js');
+    const jsonHandler = require('./jsonResponses.js');
 
-const port = process.env.PORT || process.env.NODE_PORT || 3000;
+    const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const urlStruct = {
-  '/': htmlHandler.getIndex,
-  '/style.css': htmlHandler.getCss,
-  '/success': jsonHandler.success,
-  '/badRequest': jsonHandler.badRequest,
-  '/unauthorized': jsonHandler.unauthorized,
-  '/forbidden': jsonHandler.forbidden,
-  '/internal': jsonHandler.internal,
-  '/notImplemented': jsonHandler.notImplemented,
-  notFound: jsonHandler.notFound,
-};
+    const urlStruct = {
+      '/': htmlHandler.getIndex,
+      '/style.css': htmlHandler.getCss,
+      '/success': jsonHandler.success,
+      '/badRequest': jsonHandler.badRequest,
+      '/unauthorized': jsonHandler.unauthorized,
+      '/forbidden': jsonHandler.forbidden,
+      '/internal': jsonHandler.internal,
+      '/notImplemented': jsonHandler.notImplemented,
+      notFound: jsonHandler.notFound,
+    };
 
-const onRequest = (request, response) => {
-  const parsedUrl = url.parse(request.url);
-  const params = query.parse(parsedUrl.query);
+    const onRequest = (request, response) => {
+      const parsedUrl = url.parse(request.url);
+      const params = parsedUrl.query;
 
-  if (urlStruct[parsedUrl.pathname]) {
+      if (urlStruct[parsedUrl.pathname]) {
 
-    urlStruct[parsedUrl.pathname](request, response, params);
-  } else {
+        urlStruct[parsedUrl.pathname](request, response, params);
+      } else {
 
-    urlStruct.notFound(request, response, params);
-  }
-};
+        urlStruct.notFound(request, response, params);
+      }
+    };
 
-http.createServer(onRequest).listen(port);
-console.log(`Listening on 127.0.0.1: ${port}`);
+    http.createServer(onRequest).listen(port);
+    console.log(`Listening on 127.0.0.1: ${port}`);
